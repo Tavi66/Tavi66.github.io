b3614a19d2dc0f8c5f68efe370895f17fc59ce78
Vi Ta <vta666.vt@gmail.com>
2019-07-31 16:24:43 -0500

    Updated CSS and layout.

---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+document.getElementById('navbar').innerHTML = 
+'
+    <ul style="list-style-type:none;">
+        <li class="nav-item">
+            <a href="index.html"> HOME </a>
+        </li>
+        <li class="nav-item">
+            <a href="#about"> ABOUT </a>
+        </li>
+        <li class="nav-item">
+            <a href="#skills"> SKILLS </a>
+        </li>
+        <li class="nav-item">
+            <a href="#blog"> BLOG </a>
+        </li>
+        <li class="nav-item">
+            <a href="#contact"> CONTACT </a>
+        </li>
+        <li class="nav-item left-item">
+                <a href="#"> LinkedIn </a>
+        </li>            
+        <li class="nav-item left-item">
+                <a href="https://github.com/Tavi66/"> <img id="icon" src="img/GitHub-Mark-64px.png">  GitHub </a>
+        </li>    
+    </ul>
+'
