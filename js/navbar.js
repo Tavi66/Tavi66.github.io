b156a868c50b33f8e3ccf0e293068c28ca47fe15
Vi Ta <vta666.vt@gmail.com>
2020-01-13 11:45:48 -0600

    Added seperate section pages. Add href links respectively. Added project sample images and demo file. Added Resume. Fixed navbar.js linking issues.

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,32 @@
 document.getElementById('navbar').innerHTML = 
-'
-    <ul style="list-style-type:none;">
-        <li class="nav-item">
-            <a href="index.html"> HOME </a>
-        </li>
-        <li class="nav-item">
-            <a href="#about"> ABOUT </a>
-        </li>
-        <li class="nav-item">
-            <a href="#skills"> SKILLS </a>
-        </li>
-        <li class="nav-item">
-            <a href="#blog"> BLOG </a>
-        </li>
-        <li class="nav-item">
-            <a href="#contact"> CONTACT </a>
-        </li>
-        <li class="nav-item left-item">
-                <a href="#"> LinkedIn </a>
-        </li>            
-        <li class="nav-item left-item">
-                <a href="https://github.com/Tavi66/"> <img id="icon" src="img/GitHub-Mark-64px.png">  GitHub </a>
-        </li>    
-    </ul>
-'
+`<ul style="list-style-type:none;">
+<li class="nav-item">
+  <a href="index.html"> <i class="fa fa-home"></i> Home </a>
+</li>
+<li class="nav-item">
+  <a href="/about.html"> <i class="fa fa-info-circle"></i> About </a>
+</li>
+<!-- <li class="nav-item">
+    <a href="#skills"> SKILLS </a>
+</li> -->
+<li class="nav-item">
+  <a href="projects.html"> <i class="fa fa-code"></i> Projects </a>
+</li>
+<!--<li class="nav-item">-->
+<!--    <a href="#blog"> BLOG </a>-->
+<!--</li>-->
+<li class="nav-item">
+  <a href="/src/resume_UPDATED_12_2019.docx"><i class="fa fa-file"></i> Resume </a>
+</li>
+<li class="nav-item left-item">
+  <a href="https://www.linkedin.com/in/vi-ta-39067013a/"> <i class="fa fa-linkedin-square"></i> LinkedIn </a>
+</li>
+<li class="nav-item left-item">
+  <a href="https://github.com/Tavi66/">
+    <i class="fa fa-github"></i> GitHub
+  </a>
+</li>
+</ul>
+`
+
+
